0bbbc3e722f42c9d318df81f1497b5a06f9b5052
Scott Kuroda <scott.kuroda@gmail.com>
2012-10-24 15:33:36 -0700

    Add color key as regex option

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PersistentRegexHighlight
 
-This plugin will allow you to  create regular expressions that will highlight for all documents.
+This plugin will allow you to create regular expressions that will highlight for all documents.
 
 # Installation
 Download the zip or clone the git repository. Then move it into the Packages directory.
@@ -10,15 +10,8 @@ Download the zip or clone the git repository. Then move it into the Packages dir
 ## Settings
 `regex`:
 
-An array of hash entries used to determine what to highlight. Entries have one required (`pattern`) and two optional keys (`color_scope` and `ignore_case`). The `pattern` key should map to a string specifying the patter to search for. The `color_scope` key contains the coloring scope to use. For more information on coloring, see [Specifiying Highlight Color](https://github.com/skuroda/PersistentRegexHighlight#specifying-highlight-color). The `ignore_case` key is a boolean value used to specify if case should be ignored for a particular pattern. By default, this is set to false.
+An array of hash entries used to determine what to highlight. For more information about these entries, please see [Regex Settings](https://github.com/skuroda/PersistentRegexHighlight#regex-settings).
 
-    {
-        "regex": [{
-            "pattern": "foo",
-            "color_scope": "color.scope.name",
-            "ignore_case: true
-        }]
-    }
 
 `enabled`:
 
@@ -32,9 +25,43 @@ A boolean value that specifies if highlighting should occur when a view is loade
 
 A boolean value that specifies if highlighting should occur as modifications are made.
 
-`on_focus`:
+## Regex Settings
+`pattern`:
 
-A boolean value that specifies if highlighting should occur when a window gains input focus.
+A required string parameter used to specify the pattern to match. 
+
+`ignore_case`:
+
+An optional boolean value specifiying if the pattern should ignore case. By default, this is set to false.
+
+`color_scope`:
+
+An optional parameter specifying the color scope to use. Please note you should specify **either** `color` or `color_scope`. If both are specified, `color_scope` will be taken over `color`.  This scope should already exist in your color scheme file. For more information, see [Specifiying Highlight Color](https://github.com/skuroda/PersistentRegexHighlight#specifying-highlight-color). 
+
+`color`:
+
+An optional parameter specifying a highlight color. Please note you should specify **either** `color` or `color_scope`. If both are specified, `color_scope` will be taken over `color`. The value for this entry should be in the form `RRBBGG`. By specifying this value, a new color scheme file will be generated. Please see [Specifiying Highlight Color](https://github.com/skuroda/PersistentRegexHighlight#specifying-highlight-color) for more information about specifying a color and the generated file.
+
+### Sample Regex Entries
+Example specifying a color scope.
+
+    {
+        "regex": [{
+            "pattern": "[Ff]oo",
+            "color_scope": "color.scope.name",
+            "ignore_case: true
+        }]
+    }
+
+Example specifying a color.
+
+    {
+        "regex": [{
+            "pattern": "Bar",
+            "color": "00FF00",
+            "ignore_case: false
+        }]
+    }
 
 ## Project Specific Settings
 All of the above settings can also be specified as part of the project specific settings. These values override any previous values set by higher level settings (user and default). For example, specifying a new `regex` entry will only highlight entries specified as part of the project specific settings. 
@@ -48,7 +75,10 @@ All of the above settings can also be specified as part of the project specific 
     }
 
 ## Specifying Highlight Color
-Highlight color is specified by a scope name in the color scheme files. The scope (`regex.highlight.one`) in the sample below, is the string that should be used to specify a specific coloring for a particular entry.
+You may specify colors in two ways. You may either specify a color scope or a hex color. See [Regex Settings](https://github.com/skuroda/PersistentRegexHighlight#regex-settings) for information on color formatting.
+
+### Specifying Scope
+Below is a sample entry for specifying a custom scope in the theme file. The scope 'regex.highlight.one' would be used to specify utilization of this coloring.
 
     <dict>
         <key>name</key>
@@ -61,3 +91,14 @@ Highlight color is specified by a scope name in the color scheme files. The scop
             <string>#75715E</string>
         </dict>
     </dict>
+
+### Specifying Color
+Specifying a value for the `color` key will generate a new color scheme file. This will take the contents of your current color scheme file and write it to `Packages/User/ColorScheme/<color_scheme_name_here>`. This is done to prevent polluting the default color scheme with the additional values. When this is done, you will be prompted with a dialog box to optionally change your color scheme file to the newly created one. 
+
+# Troubleshooting
+If you are having unexpected behaviors, please do not hesitate to create an issue. When you do, please include the console output, accessible through `View -> Show Console` or `Ctrl/Cmd + \`` by default.
+
+## Specifying a color
+The current libraries included with Sublime Text 2 for some platforms are missing the pyexpat module. To temporarily fix this issue, you will need to place the Python library into the Sublime Library directory. If you already have Python 2.6, like `ln -s /usr/lib/python2.6 [Sublime Text 2 Directory]/lib`. 
+
+For some platforms (tested on Ubuntu 12.04), the Python 2.6 library is no longer available. You may need to find a distribution for yourself for your platform. For Ubuntu users, you may go to [Ubuntu Archives](http://packages.ubuntu.com/lucid/python2.6). You will then need to extract the files `dpkg-deb -x python2.6_2.6.5-1ubuntu6_i386.deb python2.6`. Finally, move `usr/lib/python2.6` from the extracted files to `[Sublime Text 2 Directory]/lib`
--- persistent_regex_highlight.py
+++ persistent_regex_highlight.py
@@ -1,16 +1,20 @@
 import sublime
 import sublime_plugin
+import plistlib
+import os
+import re
 
 SETTINGS = [
     "regex",
     "enabled",
     "on_load",
-    "on_modify",
-    "on_focus"
+    "on_modify"
 ]
 
 
 class PersistentRegexHighlightCommand(sublime_plugin.TextCommand):
+    update_preferences = True
+
     def run(self, edit, settings={}):
         view = self.view
         key_base = "regex_highlight_"
@@ -20,7 +24,8 @@ class PersistentRegexHighlightCommand(sublime_plugin.TextCommand):
 
         self.remove_highlight(settings, key_base)
         if settings.get("enabled"):
-            color_dictionary = self.get_highlight_dictionary(settings)
+            color_dictionary, colors = self.get_highlight_dictionary(settings)
+            self.create_user_custom_theme(colors)
             self.highlight_regex(color_dictionary, key_base)
 
     def get_highlight_dictionary(self, settings):
@@ -29,7 +34,7 @@ class PersistentRegexHighlightCommand(sublime_plugin.TextCommand):
         region_set = LocalRegionSet()
         region_dictionary = {}
         color_dictionary = {}
-
+        colors = []
         # Find all entries that match a pattern
         for obj in regex_list:
             if "ignore_case" in obj and obj["ignore_case"]:
@@ -39,6 +44,9 @@ class PersistentRegexHighlightCommand(sublime_plugin.TextCommand):
 
             if "color_scope" in obj:
                 color = obj["color_scope"]
+            elif "color" in obj:
+                colors.append(obj["color"])
+                color = "highlight.color." + obj["color"].upper()
             else:
                 color = "entity.name.class"
 
@@ -56,7 +64,7 @@ class PersistentRegexHighlightCommand(sublime_plugin.TextCommand):
             else:
                 color_dictionary[color] = [region]
 
-        return color_dictionary
+        return color_dictionary, colors
 
     def highlight_regex(self, color_dictionary, key_base):
         view = self.view
@@ -79,28 +87,106 @@ class PersistentRegexHighlightCommand(sublime_plugin.TextCommand):
             else:
                 break
 
+    def add_colors_to_scheme(self, color_scheme, colors):
+        settings = color_scheme.settings
+        scope_exist = False
+        updates_made = False
+
+        for color in colors:
+            if re.match(r"[0-9a-fA-F]{6}", color) == None:
+                print "PersistentRegexHighlight: Invalid color specified - " + color
+                print "Colors should be in the form 'RRGGBB'"
+                continue
+            scope = "highlight.color." + color.upper()
+
+            for setting in settings:
+                if "scope" in setting and setting["scope"] == scope:
+                    scope_exist = True
+                    break
+
+            if not scope_exist:
+                updates_made = True
+                entry = {}
+                entry["name"] = "Highlight Color " + color
+                entry["scope"] = scope
+                entry["settings"] = {"foreground": "#" + color}
+                settings.append(entry)
+
+        return updates_made, color_scheme
+
+    def create_custom_color_scheme_directory(self):
+        package_path = sublime.packages_path()
+        path = package_path + "/User/ColorScheme"
+
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+        return path
+
+    def create_user_custom_theme(self, colors):
+        if len(colors) == 0:
+            return
+
+        package_path = sublime.packages_path()
+        preferences = sublime.load_settings("Preferences.sublime-settings")
+        color_scheme_base = preferences.get('color_scheme')
+
+        last_slash_index = color_scheme_base.rfind("/")
+
+        if last_slash_index != -1:
+            color_scheme_name = color_scheme_base[color_scheme_base.rfind("/"):][1:]
+        else:
+            color_scheme_name = color_scheme_base
+
+        if color_scheme_base[0:9] == "Packages/":
+            color_scheme_file = package_path + "/" + color_scheme_base[9:]
+        else:
+            color_scheme_file = package_path + "/" + color_scheme_base
+
+        try:
+            color_scheme = plistlib.readPlist(color_scheme_file)
+        except:
+            sublime.error_message("An error occured while reading color scheme file. Please check the console for details.")
+            raise
+
+        updates_made, color_scheme = self.add_colors_to_scheme(color_scheme, colors)
+
+        custom_color_path = self.create_custom_color_scheme_directory()
+        short_color_path = custom_color_path.lstrip(package_path)
+        if short_color_path[0] == "/":
+            color_scheme_entry = "Packages" + short_color_path + "/" + color_scheme_name
+        else:
+            color_scheme_entry = "Packages/" + short_color_path + "/" + color_scheme_name
+
+        if updates_made or color_scheme_base != color_scheme_entry:
+            plistlib.writePlist(color_scheme, custom_color_path + "/" + color_scheme_name)
+
+        if color_scheme_base != color_scheme_entry:
+            if PersistentRegexHighlightCommand.update_preferences:
+                okay = sublime.ok_cancel_dialog("Would you like to change your color scheme to '" + \
+                    color_scheme_entry + "'? This is where the custom colors are being saved. By " +
+                    "clicking cancel you will not be reminded again in this session")
+
+                if okay:
+                    preferences.set("color_scheme", color_scheme_entry)
+                    sublime.save_settings("Preferences.sublime-settings")
+                else:
+                    PersistentRegexHighlightCommand.update_preferences = False
+
 
 class PersistentRegexHighlightEvents(sublime_plugin.EventListener):
+
     def on_load(self, view):
         settings = get_settings(view)
-
         if settings.get("on_load"):
             view.run_command("persistent_regex_highlight", {"settings": settings})
 
     def on_modified(self, view):
         settings = get_settings(view)
-
         if settings.get("on_modify"):
             view.run_command("persistent_regex_highlight", {"settings": settings})
 
-    def on_activated(self, view):
-        settings = get_settings(view)
 
-        if settings.get("on_focus"):
-            view.run_command("persistent_regex_highlight", {"settings": settings})
-
-
-# Should remove entries wrapped in other entries
 class LocalRegionSet():
     def __init__(self):
         self.local_set = []
@@ -151,3 +237,18 @@ def get_settings(view):
             print "PersistentRegexHighlight: Invalid key '" + key + "' in project settings."
 
     return local_settings
+
+# class PersistentRegexHighlightCommand
+#   Highlight current file
+# class PersistentRegexEvents
+#   Event listeners
+#   Add on save to update all or perhaps just settings files?
+# class PersistentHighlightAllCommand
+#   Highlight all all open files
+# class ManageHighlight
+#   Highlight Views
+#   Remove highlight from views
+# class ManageColorEntries
+#   Class should be responsible for managing adding colors to a theme
+# class Set
+#   Keep list of regions, ensuring none are encapsulatd by another region
